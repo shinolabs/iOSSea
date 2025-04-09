d470de1612cd3e733d01204d1d802806f9efa643
makrowave <maksstyzej@gmail.com>
2025-04-09 18:42:46 +0200

    refactor: remove extra NavigationView - app looks normal on iPad

--- iOSSea/ContentView.swift
+++ iOSSea/ContentView.swift
@@ -11,62 +11,60 @@ struct ContentView: View {
     @State private var selectedTab: Int = 0
     @StateObject private var viewModel : ContentViewModel = ContentViewModel()
     var body: some View {
-        NavigationView {
-            TabView(selection: $selectedTab) {
-                Tab("", systemImage: "house", value: 0) {
-                    NavigationStack {
-                        HomeView()
-                            .navigationTitle("Home")
-                            .toolbarVisibility(.visible)
-                            .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .automatic)
-                            .toolbarBackground(.visible, for: .automatic)
-                            .toolbar {
-                                if viewModel.isLoggedIn {
-                                    NavigationLink(destination: PainterView()) {
-                                        Image(systemName: "plus.square")
-                                    }
-                                }
-                            }
-                    }
-                    .toolbarBackground(.visible, for: .tabBar)
-                    .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
-                }
-                Tab("", systemImage: "magnifyingglass", value: 1) {
-                    NavigationStack{
-                        SearchView()
-                            .navigationTitle("Search")
-                            .toolbarVisibility(.visible)
-                            .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .automatic)
-                            .toolbarBackground(.visible, for: .automatic)
-                            
-                    }
-                    .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
-                    .toolbarBackground(.visible, for: .tabBar)
-                }
-                Tab("", systemImage: "person.crop.circle.fill", value: 2) {
-                    NavigationStack{
-                        Group {
-                            if !viewModel.isLoggedIn {
-                                LoginView()
-                            } else {
-                                ProfileView(did: AuthenticationManager.shared.getDid()!, ownProfile: true)
-                            }
-                        }
-                        .navigationTitle(!viewModel.isLoggedIn ? "Log in" : "Profile")
-                        .navigationBarTitleDisplayMode(.large)
+        TabView(selection: $selectedTab) {
+            Tab("", systemImage: "house", value: 0) {
+                NavigationStack {
+                    HomeView()
+                        .navigationTitle("Home")
                         .toolbarVisibility(.visible)
                         .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .automatic)
                         .toolbarBackground(.visible, for: .automatic)
                         .toolbar {
-                            NavigationLink(destination: SettingsView()) {
-                                Image(systemName: "gear")
+                            if viewModel.isLoggedIn {
+                                NavigationLink(destination: PainterView()) {
+                                    Image(systemName: "plus.square")
+                                }
                             }
                         }
-                            
-                    }
-                    .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
-                    .toolbarBackground(.visible, for: .tabBar)
                 }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
+            }
+            Tab("", systemImage: "magnifyingglass", value: 1) {
+                NavigationStack{
+                    SearchView()
+                        .navigationTitle("Search")
+                        .toolbarVisibility(.visible)
+                        .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .automatic)
+                        .toolbarBackground(.visible, for: .automatic)
+                        
+                }
+                .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
+                .toolbarBackground(.visible, for: .tabBar)
+            }
+            Tab("", systemImage: "person.crop.circle.fill", value: 2) {
+                NavigationStack{
+                    Group {
+                        if !viewModel.isLoggedIn {
+                            LoginView()
+                        } else {
+                            ProfileView(did: AuthenticationManager.shared.getDid()!, ownProfile: true)
+                        }
+                    }
+                    .navigationTitle(!viewModel.isLoggedIn ? "Log in" : "Profile")
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbarVisibility(.visible)
+                    .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .automatic)
+                    .toolbarBackground(.visible, for: .automatic)
+                    .toolbar {
+                        NavigationLink(destination: SettingsView()) {
+                            Image(systemName: "gear")
+                        }
+                    }
+                        
+                }
+                .toolbarBackground(Color(UIColor(named: "Foreground")!), for: .tabBar)
+                .toolbarBackground(.visible, for: .tabBar)
             }
         }
         .onReceive(AuthenticationManager.shared.eventSubject) { event in
