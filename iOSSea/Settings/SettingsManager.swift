8347d3886a51876820da7c6843f1defaa5eb4f55
makrowave <maksstyzej@gmail.com>
2025-04-10 13:37:12 +0200

    feat: save theme setting to CoreData

--- iOSSea/Settings/SettingsManager.swift
+++ iOSSea/Settings/SettingsManager.swift
@@ -6,9 +6,62 @@
 //
 
 import SwiftUI
-
+import CoreData
 class SettingsManager: ObservableObject {
+    let context = PersistenceController.shared.container.viewContext
     public static let shared = SettingsManager()
     
-    @Published var theme: ColorScheme?
+    @Published var theme: ColorScheme? {
+        didSet {
+            var theme: String?
+            switch self.theme {
+            case .light:
+                theme = "light"
+            case .dark:
+                theme = "dark"
+            default:
+                theme = "auto"
+            }
+            saveOrUpdateSetting(key: "theme", value: theme!)
+        }
+    }
+    
+    init() {
+        let themeSetting = fetchSetting(key: "theme")
+        guard let theme = themeSetting else {return}
+        switch theme.value {
+        case "light":
+            self.theme = .light
+        case "dark":
+            self.theme = .dark
+        default:
+            self.theme = nil
+        }
+    }
+    
+    func fetchSetting(key: String) -> Setting? {
+        let request: NSFetchRequest<Setting> = Setting.fetchRequest()
+        request.predicate = NSPredicate(format: "key == %@", key)
+        request.fetchLimit = 1  // Get only one result
+
+        do {
+            return try context.fetch(request).first
+        } catch {
+            print("Failed to fetch setting for key \(key): \(error)")
+            return nil
+        }
+    }
+
+    func saveOrUpdateSetting(key: String, value: String) {
+        let setting = fetchSetting(key: key) ?? Setting(context: context) // Fetch or create new
+        setting.key = key
+        setting.value = value
+
+        do {
+            try context.save()
+            print("Setting saved/updated successfully!")
+        } catch {
+            print("Failed to save setting: \(error)")
+        }
+    }
 }
